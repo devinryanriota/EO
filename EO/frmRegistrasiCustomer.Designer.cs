730b88dc09397be376ffcaa9a47f12791650cc63
devinryanriota <devinryanriota@gmail.com>
2016-05-18 15:51:22 +0700

    18 May 16.00

---
+++ EO/frmRegistrasiCustomer.Designer.cs
@@ -0,0 +1,197 @@
+﻿namespace EO
+{
+    partial class frmRegistrasiCustomer
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pnlHeader = new System.Windows.Forms.Panel();
+            this.lblHeader = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.txtNama = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtAlamat = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtNoTelepon = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtEmail = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btnRegister = new System.Windows.Forms.Button();
+            this.pnlHeader.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pnlHeader
+            // 
+            this.pnlHeader.Controls.Add(this.lblHeader);
+            this.pnlHeader.Dock = System.Windows.Forms.DockStyle.Top;
+            this.pnlHeader.Location = new System.Drawing.Point(0, 0);
+            this.pnlHeader.Name = "pnlHeader";
+            this.pnlHeader.Size = new System.Drawing.Size(584, 65);
+            this.pnlHeader.TabIndex = 4;
+            // 
+            // lblHeader
+            // 
+            this.lblHeader.AutoSize = true;
+            this.lblHeader.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblHeader.Location = new System.Drawing.Point(172, 20);
+            this.lblHeader.Name = "lblHeader";
+            this.lblHeader.Size = new System.Drawing.Size(231, 29);
+            this.lblHeader.TabIndex = 0;
+            this.lblHeader.Text = "Registrasi Customer";
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.txtEmail);
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.txtNoTelepon);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.txtAlamat);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.txtNama);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(0, 71);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(584, 202);
+            this.panel1.TabIndex = 1;
+            // 
+            // txtNama
+            // 
+            this.txtNama.Location = new System.Drawing.Point(211, 22);
+            this.txtNama.Name = "txtNama";
+            this.txtNama.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtNama.Size = new System.Drawing.Size(314, 20);
+            this.txtNama.TabIndex = 9;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(48, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(45, 16);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Nama";
+            // 
+            // txtAlamat
+            // 
+            this.txtAlamat.Location = new System.Drawing.Point(211, 54);
+            this.txtAlamat.Multiline = true;
+            this.txtAlamat.Name = "txtAlamat";
+            this.txtAlamat.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtAlamat.Size = new System.Drawing.Size(314, 58);
+            this.txtAlamat.TabIndex = 11;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(48, 55);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(50, 16);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "Alamat";
+            // 
+            // txtNoTelepon
+            // 
+            this.txtNoTelepon.Location = new System.Drawing.Point(211, 123);
+            this.txtNoTelepon.Name = "txtNoTelepon";
+            this.txtNoTelepon.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtNoTelepon.Size = new System.Drawing.Size(314, 20);
+            this.txtNoTelepon.TabIndex = 13;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(48, 124);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(103, 16);
+            this.label3.TabIndex = 12;
+            this.label3.Text = "Nomor Telepon";
+            // 
+            // txtEmail
+            // 
+            this.txtEmail.Location = new System.Drawing.Point(211, 152);
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtEmail.Size = new System.Drawing.Size(314, 20);
+            this.txtEmail.TabIndex = 15;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(48, 153);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(42, 16);
+            this.label4.TabIndex = 14;
+            this.label4.Text = "Email";
+            // 
+            // btnRegister
+            // 
+            this.btnRegister.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.btnRegister.Location = new System.Drawing.Point(0, 274);
+            this.btnRegister.Name = "btnRegister";
+            this.btnRegister.Size = new System.Drawing.Size(584, 51);
+            this.btnRegister.TabIndex = 5;
+            this.btnRegister.Text = "REGISTER";
+            this.btnRegister.UseVisualStyleBackColor = true;
+            // 
+            // frmRegistrasiCustomer
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(584, 325);
+            this.Controls.Add(this.btnRegister);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.pnlHeader);
+            this.Name = "frmRegistrasiCustomer";
+            this.Text = "Form Registrasi Customer";
+            this.pnlHeader.ResumeLayout(false);
+            this.pnlHeader.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel pnlHeader;
+        private System.Windows.Forms.Label lblHeader;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TextBox txtNama;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtAlamat;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtNoTelepon;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtEmail;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button btnRegister;
+    }
+}
